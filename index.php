e4340220dd23d11a2a84e460c211f7c024202130
pilukarts <pilarrodriguezcastillejo@gmail.com>
2025-05-26 23:51:12 +0100

    Restaurando carpetas y archivos desde commit e013942

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $request = $_SERVER['REQUEST_URI'];
 
@@ -9,15 +8,3 @@ if (file_exists(__DIR__ . $request)) {
 
 // Redirect all other requests to your SPA (index.html)
 include 'index.html';
-=======
-<?php
-$request = $_SERVER['REQUEST_URI'];
-
-// If the requested file exists, serve it
-if (file_exists(__DIR__ . $request)) {
-    return false;
-}
-
-// Redirect all other requests to your SPA (index.html)
-include 'index.html';
->>>>>>> c8222c1d04fc89527f055b9312c0a349ba069a83
--- phpinfo.php
+++ phpinfo.php
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 <?php
 phpinfo();
 ?>
-=======
-<?php
-phpinfo();
-?>
->>>>>>> c8222c1d04fc89527f055b9312c0a349ba069a83
